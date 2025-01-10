34b57cd10e12bb52d539d045f699a9a149e4f83c
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-01-10 16:50:35 +0530

    update

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+
+const apiKeys = [process.env.API_KEY];
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers["api-key"]; // Get API key from request headers
+
+  if (!apiKey || !apiKeys.includes(apiKey)) {
+    return res.status(403).json({
+      warning: "This is REACT-NATIVES BACKEND - you should not be here :)",
+    });
+  }
+
+  next(); // Proceed to the next middleware or route handler
+};
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "api-key"],
+  })
+);
+
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// Basic route to test server
+app.use("/api", apiKeyMiddleware);
+app.get("/api/hii", (req, res) => {
+  res.send("API is running... develop by shubham ");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
