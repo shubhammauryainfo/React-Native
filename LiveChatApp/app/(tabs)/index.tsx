43d9484fbac8dbd81b53ca27e8de9f4300f3eb5e
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-01-12 00:11:47 +0530

    update

--- LiveChatApp/app/(tabs)/index.tsx
+++ LiveChatApp/app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome! Shubham ❤️</ThemedText>
+        <ThemedText type="title">Welcome!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
