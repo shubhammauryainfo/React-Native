eb8223ec9d281d5d785f7c076c8ff162e6ac6747
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-05-09 23:50:21 +0530

    update

--- frontend/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Test() {
-  return (
-    <View className="bg-red-500 p-4 rounded-xl">
-      <Text className="text-white font-bold">Tailwind works!</Text>
-    </View>
-  );
-}
--- frontend/config/env.ts
+++
@@ -1,4 +0,0 @@
-import Constants from 'expo-constants';
-
-export const API_URL = Constants.expoConfig?.extra?.API_URL as string;
-export const API_KEY = Constants.expoConfig?.extra?.API_KEY as string;
