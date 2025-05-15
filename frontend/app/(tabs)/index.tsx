a628efc370d11e03b0b41c45cb6c8cb4af4051e2
shubham maurya <131405109+shubhammauryainfo@users.noreply.github.com>
2025-05-16 03:03:35 +0530

    update

---
+++ frontend/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center bg-red-100">
+      <Text className="text-2xl font-bold text-blue-500">Hello Tailwind 👋</Text>
+    </View>
+  );
+}
